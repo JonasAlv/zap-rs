7fc71d51b4acc24589ef0ec23c7a74e5c685b23f
Jonas Alves <jonas001san@gmail.com>
2025-07-07 01:48:32 -0300

    testing new features

--- src/tray.rs
+++ src/tray.rs
@@ -1,11 +1,12 @@
 use tauri::menu::{Menu, MenuItem};
-use tauri::tray::TrayIconBuilder;
+use tauri::tray::{TrayIconBuilder, TrayIconEvent, MouseButton, MouseButtonState};
 use tauri::{App, Manager, Result};
 use tauri::image::Image;
 
 fn show_window<R: tauri::Runtime>(app_handle: &tauri::AppHandle<R>) -> Result<()> {
     if let Some(window) = app_handle.get_webview_window("main") {
         window.show()?;
+        window.set_focus()?;
     } else {
         eprintln!("Window [main] not found when trying [show_window()].");
     }
@@ -50,22 +51,34 @@ pub fn init_tray<R: tauri::Runtime>(app: &App<R>) -> Result<()> {
 
     let menu = Menu::with_items(app, &[&toggle_item, &quit_item])?;
 
+    let toggle_item_clone_for_menu = toggle_item.clone();
+    let toggle_item_clone_for_click = toggle_item.clone();
+
     TrayIconBuilder::new()
         .icon(Image::from_bytes(include_bytes!("../icons/tray_64x64.png")).expect("Failed to load tray icon"))
         .menu(&menu)
-        .on_menu_event({
-            let toggle_item = toggle_item.clone();
-            move |app_handle, event| match event.id.as_ref() {
-                "toggle" => {
-                    let _ = toggle_window(app_handle, &toggle_item);
-                }
-                "quit" => {
-                    let _ = quit_app(app_handle);
-                }
-                _ => {
-                    eprintln!("menu item {:?} not handled", event.id);
-                }
-            }       
+        .show_menu_on_left_click(false)
+        .on_menu_event(move |app_handle, event| match event.id.as_ref() {
+            "toggle" => {
+                let _ = toggle_window(app_handle, &toggle_item_clone_for_menu);
+            }
+            "quit" => {
+                let _ = quit_app(app_handle);
+            }
+            _ => {
+                eprintln!("menu item {:?} not handled", event.id);
+            }
+        })
+        .on_tray_icon_event(move |tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                let _ = toggle_window(app, &toggle_item_clone_for_click);
+            }
         })
         .build(app)?;
 
